ad5ecdc9fd5e8fbe0362edc2d63159a0b6f7ba89
destylae <destypati1122@gmail.com>
2024-11-21 07:03:36 +0700

    video13

---
+++ video13/app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, ScrollView } from 'react-native';
+import PositionReactNative from './PositionReactNative';
+
+const App = () => {
+  return (
+    <View>
+      <ScrollView>
+        <PositionReactNative />
+      </ScrollView>
+    </View>
+  );
+};
+
+export default App;
